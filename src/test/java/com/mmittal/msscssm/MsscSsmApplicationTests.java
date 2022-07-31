e62516dc8af0b7fde343d74f0834f425f22a11d6
manishmittal050 <manishmittal050@gmail.com>
2022-07-31 11:21:07 +0530

    First commit

---
+++ src/test/java/com/mmittal/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmittal.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
